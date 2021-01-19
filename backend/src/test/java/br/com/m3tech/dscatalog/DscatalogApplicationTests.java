ac5d611e8920ffe404b9032d41cacf364d8b6ae6
mackristofer <mackristofer@gmail.com>
2021-01-19 01:03:40 -0300

    Project created

---
+++ backend/src/test/java/br/com/m3tech/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.m3tech.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
