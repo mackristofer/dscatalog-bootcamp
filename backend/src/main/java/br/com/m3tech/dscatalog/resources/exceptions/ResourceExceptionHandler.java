fb302d8a694990dc7aa15fc2b9fc4ea5079ddfb6
mackristofer <mackristofer@gmail.com>
2021-01-20 17:40:21 -0300

    find category by id, exception handling

---
+++ backend/src/main/java/br/com/m3tech/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package br.com.m3tech.dscatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.m3tech.dscatalog.services.exceptions.EntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError()
+;
+		err.setTimestamp(Instant.now());
+		err.setStaus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
--- backend/src/main/java/br/com/m3tech/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/m3tech/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package br.com.m3tech.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.m3tech.dscatalog.dto.CategoryDTO;
 import br.com.m3tech.dscatalog.entities.Category;
 import br.com.m3tech.dscatalog.repositories.CategoryRepository;
+import br.com.m3tech.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,6 +24,13 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Object not found"));
+		return new CategoryDTO(entity);
 	}
 
 }
