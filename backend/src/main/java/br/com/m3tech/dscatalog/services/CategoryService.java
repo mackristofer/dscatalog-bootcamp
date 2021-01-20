72d3af1197780f89f6c5e00797acbb0771372cdb
mackristofer <mackristofer@gmail.com>
2021-01-20 02:19:49 -0300

    DTO

--- backend/src/main/java/br/com/m3tech/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/m3tech/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package br.com.m3tech.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.m3tech.dscatalog.dto.CategoryDTO;
 import br.com.m3tech.dscatalog.entities.Category;
 import br.com.m3tech.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 
 }
