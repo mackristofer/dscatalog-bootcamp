5c17367e537a54da8fdf684eeeb4fe060c606474
mackristofer <mackristofer@gmail.com>
2021-01-20 01:31:06 -0300

    Teste H2 data base

---
+++ backend/src/main/java/br/com/m3tech/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package br.com.m3tech.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.m3tech.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ backend/src/main/java/br/com/m3tech/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package br.com.m3tech.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.m3tech.dscatalog.entities.Category;
+import br.com.m3tech.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
