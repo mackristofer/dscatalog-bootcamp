2ad88424c6c7e5248d564bac94594e78bb4ceea9
mackristofer <mackristofer@gmail.com>
2021-01-20 18:06:54 -0300

    Insert category

--- backend/src/main/java/br/com/m3tech/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/br/com/m3tech/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package br.com.m3tech.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.m3tech.dscatalog.dto.CategoryDTO;
 import br.com.m3tech.dscatalog.services.CategoryService;
@@ -15,20 +19,28 @@ import br.com.m3tech.dscatalog.services.CategoryService;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
+
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll(){
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
-	@GetMapping(value="/{id}")
-	public ResponseEntity<CategoryDTO> finById(@PathVariable Long id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> finById(@PathVariable Long id) {
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+
 	}
 
 }
