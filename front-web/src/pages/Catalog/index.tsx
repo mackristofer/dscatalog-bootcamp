6bcaeeb2cdd3656148c88b78df5019a18a681a32
mackristofer <mackristofer@gmail.com>
2021-02-11 01:02:43 -0300

    Product returns api

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
+import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
-const Catalog = () => (
-<div className="catalog-container"> 
-   <h1 className="catalog-title">Catálogo de produtos</h1>
-   <div className="catalog-products">
-      <Link to="products/1"><ProductCard /></Link>
-      <Link to="products/2"><ProductCard /></Link>
-      <Link to="products/3"><ProductCard /></Link>
-      <Link to="products/4"><ProductCard /></Link>
-      <Link to="products/5"><ProductCard /></Link>
-      <Link to="products/6"><ProductCard /></Link>
+const Catalog = () => {
 
-   </div>
-</div>
-);
+   const [productsReponse, setProductsResponse] = useState<ProductsResponse>();
+   console.log(productsReponse);
+   useEffect(() => {
+      const params = {
+         page: 0,
+         linesPerPage: 10
+      }
+      makeRequest({url:'/products', params})
+      .then(response => setProductsResponse(response.data));
+
+   }, []);
+   return(
+      <div className="catalog-container"> 
+         <h1 className="catalog-title">Catálogo de produtos</h1>
+         <div className="catalog-products">
+            {productsReponse?.content.map(product => (
+            <Link to="products/1" key={product.id}>
+               <ProductCard />
+               </Link>
+               ))}
+      
+         </div>
+      </div>
+      );
+}
 
 export default Catalog;
