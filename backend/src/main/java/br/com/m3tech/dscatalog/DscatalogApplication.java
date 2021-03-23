8b67cd1a32c7efd43340f103f1240a4d70d1d988
mackristofer <mackristofer@gmail.com>
2021-03-23 17:13:07 -0300

    first upload test

--- backend/src/main/java/br/com/m3tech/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/br/com/m3tech/dscatalog/DscatalogApplication.java
@@ -1,13 +1,26 @@
 package br.com.m3tech.dscatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DscatalogApplication {
+import br.com.m3tech.dscatalog.services.S3Service;
 
+@SpringBootApplication
+public class DscatalogApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\print.jpg");
+		
 	}
 
 }
