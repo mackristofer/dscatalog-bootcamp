73d526414369f1834ba32347c1691b943202f0c2
mackristofer <mackristofer@gmail.com>
2021-03-22 22:37:31 -0300

    implementing oauth2 authorization service

--- backend/src/main/java/br/com/m3tech/dscatalog/services/UserService.java
+++ backend/src/main/java/br/com/m3tech/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +30,10 @@ import br.com.m3tech.dscatalog.services.exceptions.DataBaseException;
 import br.com.m3tech.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
-	
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
@@ -55,7 +62,7 @@ public class UserService {
 		copyDtoToEntity(dto, entity);
 		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
 		entity = repository.save(entity);
-		
+
 		return new UserDTO(entity);
 	}
 
@@ -71,8 +78,6 @@ public class UserService {
 		}
 	}
 
-
-
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
@@ -82,21 +87,32 @@ public class UserService {
 			throw new DataBaseException("Integrity violation");
 		}
 	}
-	
+
 	private void copyDtoToEntity(UserDTO dto, User entity) {
-		
+
 		entity.setFirstName(dto.getFirstName());
 		entity.setLastName(dto.getLastName());
-		
+
 		entity.setEmail(dto.getEmail());
 
 		entity.getRoles().clear();
-		
+
 		for (RoleDTO roledto : dto.getRoles()) {
 			Role role = roleRepository.getOne(roledto.getId());
 			entity.getRoles().add(role);
 		}
-		
+
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not foud: "+ username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: "+username);
+		return user;
 	}
 
 }
