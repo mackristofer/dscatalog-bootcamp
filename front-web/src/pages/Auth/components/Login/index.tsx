402d0f769863b8befd1f57b6430c5316626e9151
mackristofer <mackristofer@gmail.com>
2021-03-26 21:33:08 -0300

    create auth module

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,22 +1,39 @@
-import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import ButtonIcon from 'core/components/ButtonIcon';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    username:string;
+    password:string;
+}
+
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+      };
+
     return(
         <AuthCard title="Login">
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <input
         type="email"
         className="form-control input-base margin-bottom-30"
         placeholder="E-mail"
+        name="username"
+        ref={register}
         />
         <input
         type="password"
         className="form-control input-base"
         placeholder="Senha"
+        name="password"
+        ref={register}
         />
         <Link to="/admin/auth/recover" className="login-link-recover">Esqueci a senha?</Link>
         <div className="login-submit">
